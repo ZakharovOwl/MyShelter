ddc7129101e80cc7dd64416e471b7ff2336447bf
Aleksander <Zakharov.owl@gmail.com>
2020-10-23 23:03:45 +0300

    feat: add full burger

--- pages/main/app.js
+++ pages/main/app.js
@@ -1,1 +1,30 @@
-const burger
+// MENU MAIN Burger
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.menu');
+    const list = document.querySelector('.list');
+    const logo = document.querySelector('.logo');
+    const listLinks = document.querySelectorAll('.menu li')
+
+    burger.addEventListener('click', () => {
+        list.classList.toggle('list-active')
+        burger.classList.toggle('burger-active')
+        menu.classList.toggle('menu-burger')
+        logo.classList.toggle('logo-active')
+
+        //animation links
+ 
+
+        listLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/ 7 +0.2}s`
+            }
+        })
+    })
+
+}
+
+
+navSlide()
